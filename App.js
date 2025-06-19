cb4e76e28809df1f98486d0148ccc7260218a379
Alexandraa <wibugamersindo@gmail.com>
2025-06-19 10:56:03 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1 style={{ color: '#2b6cb0' }}>Alexandra's Portfolio</h1>
+      <p>Halo! Ini adalah portofolio online saya yang dibuat dengan React.</p>
+    </div>
+  );
+}
+
+export default App;
